9a3c284debfa4108096e8e0264d0218ea1c669cb
danieltonad <soladan2000@gmail.com>
2023-09-18 13:35:05 -0700

    modified url get to post

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from config.database import database
 from utils import uniqueShorts, is_valid_url
+from request import ShortnerRequest
 
 app = FastAPI()
 
@@ -8,13 +9,12 @@ app = FastAPI()
 def root():
     return '__init__'
 
-@app.post('/shorten/{url}')
-async def shorten_url(data: dict, request: Request):
-    if is_valid_url(data.):
-        return True
-    # short = uniqueShorts()
-    # payload = {'key': short, 'link': url}
-    # if database.put(payload, expire_in=3600):
-    #     return f'{request.base_url}{short}'
-    # else:
-    #     False
+@app.post('/shorten')
+async def shorten_url(data: ShortnerRequest, request: Request):
+    if is_valid_url(data.links):
+        short = uniqueShorts()
+        payload = {'key': short, 'link': data.links}
+        if database.put(payload, expire_in=3600):
+            return f'{request.base_url}{short}'
+        else:
+            False
--- request.py
+++ request.py
@@ -1,4 +1,4 @@
 from pydantic import BaseModel
 
-class Shortner(BaseModel):
+class ShortnerRequest(BaseModel):
     links: str
