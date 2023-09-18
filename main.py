e7d7c03216e07f7f48f01350577464e9f4326821
danieltonad <soladan2000@gmail.com>
2023-09-18 14:04:17 -0700

    completed url shortner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from config.database import database
-from utils import uniqueShorts, is_valid_url
+from utils import uniqueShorts, is_valid_url, duplicateLink, redirectShorts
 from request import ShortnerRequest
 
 app = FastAPI()
@@ -9,12 +9,21 @@ app = FastAPI()
 def root():
     return '__init__'
 
+@app.get('/{short}')
+async def links(short: str):
+    return await redirectShorts(short)
+
 @app.post('/shorten')
 async def shorten_url(data: ShortnerRequest, request: Request):
     if is_valid_url(data.links):
-        short = uniqueShorts()
+        duplicate =  await duplicateLink(data.links, request)
+        if duplicate:
+            return duplicate
+        short = await uniqueShorts()
         payload = {'key': short, 'link': data.links}
         if database.put(payload, expire_in=3600):
             return f'{request.base_url}{short}'
         else:
             False
+
+    return False
--- utils.py
+++ utils.py
@@ -1,19 +1,37 @@
 import random, string
 from config.database import database
+from fastapi.requests import Request
+from fastapi.responses import RedirectResponse
 import re
+
+
+async def redirectShorts(short: str):
+    link = database.get(short)
+    if link:
+        return RedirectResponse(link['link'])
+    return 'invalid link'
+
 
 def is_valid_url(url):
     # Regular expression for a basic URL validation
     url_pattern = re.compile(r'^(https?|http)://[^\s/$.?#].[^\s]*$')
     return re.match(url_pattern, url) is not None
 
-def uniqueShorts():
+async def uniqueShorts():
     short = randomString(5)
     if database.get(short):
-        return uniqueShorts()
+        return await uniqueShorts()
     else:
          return short
 
 def randomString(length=10):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
+
+async def duplicateLink(link: str, request: Request):
+    __ = database.fetch({'link': link})._items
+    if __:
+        short = __[0]
+        return f'{request.base_url}{short["key"]}'
+    else:
+         return False
